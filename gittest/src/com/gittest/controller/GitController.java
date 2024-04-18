372976d29de8b36a7606e29e7f02dd66eccdb8be
User <User@DESKTOP-38P1SBN>
2024-04-18 12:20:43 +0900

    bs_GittestController.test메소드 추가 별찍기

--- gittest/src/com/gittest/controller/GitController.java
+++
@@ -1,5 +0,0 @@
-package com.gittest.controller;
-
-public class GitController {
-
-}
---
+++ gittest/src/com/gittest/controller/GitTestController.java
@@ -0,0 +1,8 @@
+package com.gittest.controller;
+
+public class GitTestController {
+	public void test() {
+		System.out.println("별찍기");
+		
+	}
+}
